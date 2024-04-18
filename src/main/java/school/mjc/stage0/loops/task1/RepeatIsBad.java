f7da6837774b861a66bfdc267c08a9680f734e33
afdansx <afafmaljfmajkfamkl@gmail.com>
2024-04-19 00:12:10 +0300

    solved

--- src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
+++ src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
@@ -4,6 +4,47 @@ public class RepeatIsBad {
 
     @SuppressWarnings("java:S106")
     public void repeatIsBad(){
-
+        int i = 0;
+        if(i <= 21){
+            System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+        }if(i <= 21){
+            System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+        }if(i <= 21){
+            System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+        }if(i <= 21){
+            System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+        }if(i <= 21){
+            System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+        }if(i <= 21){
+            System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+        }if(i <= 21){
+            System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+        }if(i <= 21){
+            System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+        }if(i <= 21){
+            System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+        }if(i <= 21){
+            System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+        }if(i <= 21){
+            System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+        }if(i <= 21){
+            System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+        }if(i <= 21){
+            System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+        }if(i <= 21){
+            System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+        }if(i <= 21){
+            System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+        }if(i <= 21){
+            System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+        }if(i <= 21){
+            System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+        }if(i <= 21){
+            System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+        }if(i <= 21){
+            System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+        }if(i <= 21){
+            System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+        }
     }
 }
